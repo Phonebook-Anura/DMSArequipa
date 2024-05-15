3db4a2bea2533b5102460657d08667e2dcd1f492
RobertTorresGuerra <rtorres@anura.pe>
2024-05-15 15:31:33 -0500

    Create Yealink.php

---
+++ Yealink.php
@@ -0,0 +1,68 @@
+<YealinkIPPhoneBook>
+<Title>Phonebook</Title>
+<Menu Name="PERSONAL AREQUIPA">
+<Unit Name="ARNOLD MUÑOZ" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="217"/>
+<Unit Name="BENITO PONCE LASTRA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="201"/>
+<Unit Name="CECILIA BLAS CASTRO " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="228"/>
+<Unit Name="RECURSOS HUMANOS" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="246"/>
+<Unit Name="DANY APOLAYA SARAVIA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="203"/>
+<Unit Name="DANY VILLANUEVA JAUREGUI" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="202"/>
+<Unit Name="DEYANIRA VILLANUEVA JAUREGUI" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="209"/>
+<Unit Name="DIEGO RUIZ AQUINO" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="260"/>
+<Unit Name="ELIZABETH CHIRINOS OTINIANO " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="261"/>
+<Unit Name="ELMER CUIPAL PEREA " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="270"/>
+<Unit Name="LUIS FERNANDEZ VARGAS" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="253"/>
+<Unit Name="FERNANDO TREJO AQUISE" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="252"/>
+<Unit Name="JAIME CHALEN" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="254"/>
+<Unit Name="CARLOS VITE INCA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="517"/>
+<Unit Name="JHENNYFER GASPAR CULCAS" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="284"/>
+<Unit Name="JOHN VENTURA MOLINA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="575"/>
+<Unit Name="FRANCO MARIÑO" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="223"/>
+<Unit Name="JOSE ESTRADA ZAPATA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="532"/>
+<Unit Name="JOSE PALOMINO TIPULA " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="281"/>
+<Unit Name="LUIS SANES AHUANARI " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="295"/>
+<Unit Name="LUIS LAFORE ANTON" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="204"/>
+<Unit Name="LUIS FERNANDEZ VARGAS " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="282"/>
+<Unit Name="NANCY GARCIA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="225"/>
+<Unit Name="CECILIA BLAS CASTRO" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="262"/>
+<Unit Name="PAUL SANCHEZ HERNANDEZ" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="257"/>
+<Unit Name="RECEPCION DMS" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="200"/>
+<Unit Name="ROSA LEON JULCAMAYAN " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="266"/>
+<Unit Name="SALA GENERAL DMS" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="280"/>
+<Unit Name="SALA RECEPCION " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="236"/>
+<Unit Name="SALA SISTEMAS " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="213"/>
+<Unit Name="SEGURIDAD AREQUIPA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="251"/>
+<Unit Name="ANA BUSTAMANTE MACHACCA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="410"/>
+<Unit Name="VALERY DELGADO PAUCARMAYTA " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="272"/>
+<Unit Name="VERONICA JULIANA CRISOSTOMO MEDINA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="273"/>
+<Unit Name="VICTOR DE LOS RIOS RAMIREZ" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="210"/>
+<Unit Name="DANIEL CHIRINOS OTINIANO " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="243"/>
+<Unit Name="VIOLETA OTINIANO JAUREGUI" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="258"/>
+<Unit Name="WILSON BALLONA INGA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="279"/>
+
+</Menu>
+<Menu Name="PERSONAL CHINCHON">
+<Unit Name="ANDREA HALANOCCA CONOVILCA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="241"/>
+<Unit Name="ALMACEN ZEBRA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="515"/>
+<Unit Name="ARTURO MARCOS BUSTINZA " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="545"/>
+<Unit Name="CERINIO ROSARIO ROMERO " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="628"/>
+<Unit Name="DARIO NINAN CUSICUNA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="552"/>
+<Unit Name="DIEGO DURAND ROJAS" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="556"/>
+<Unit Name="SEGURIDAD CHINCHON" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="570"/>
+<Unit Name="JORGE GAMBOA RISCO" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="567"/>
+<Unit Name="JUAN CARLOS ROSARIO SALAZAR " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="518"/>
+<Unit Name="KASANDRA ORMEÑO" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="403"/>
+<Unit Name="MANUEL LLERENA ZAMORA " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="267"/>
+<Unit Name="MERY ROMERO VIDAURRE" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="214"/>
+<Unit Name="OMAR ZEVALLOS VILLANUEVA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="569"/>
+<Unit Name="RAQUEL SUCAPUCA MENDOZA" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="271"/>
+<Unit Name="JUAN TUEROS ROBLES" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="247"/>
+<Unit Name="WILLIAM PECEROS ALVARADO " default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="519"/>
+<Unit Name="RAUL BOCANEGRA GUZMAN" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="548"/>
+<Unit Name="RENZO LIZA CARRILLO" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="550"/>
+</Menu>
+<Menu Name="CORREO DE VOZ">
+<Unit Name="Mi Correo de Voz" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="*869"/>
+<Unit Name="Correo de Voz" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="333"/>
+</Menu>
+</YealinkIPPhoneBook>
